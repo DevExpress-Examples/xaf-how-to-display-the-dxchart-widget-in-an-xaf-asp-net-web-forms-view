63a420a9e65e233bae02cd178f137a17b3df623d
andrey <andrey.kozhevnikov@devexpress.com>
2023-10-25 13:40:28 +0400

    init new

---
+++ CS/WebChart/WebChart.Web/WebApplication.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Web;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Xpo;
+
+namespace WebChart.Web {
+    // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Web.WebApplication
+    public partial class WebChartAspNetApplication : WebApplication {
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule module2;
+        private WebChart.Module.WebChartModule module3;
+        private WebChart.Module.Web.WebChartAspNetModule module4;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule objectsModule;
+
+        public WebChartAspNetApplication() {
+            InitializeComponent();
+        }
+        protected override IViewUrlManager CreateViewUrlManager() {
+            return new ViewUrlManager();
+        }
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            args.ObjectSpaceProvider = new XPObjectSpaceProvider(GetDataStoreProvider(args.ConnectionString, args.Connection), true);
+            args.ObjectSpaceProviders.Add(new NonPersistentObjectSpaceProvider(TypesInfo, null));
+        }
+        private IXpoDataStoreProvider GetDataStoreProvider(string connectionString, System.Data.IDbConnection connection) {
+            System.Web.HttpApplicationState application = (System.Web.HttpContext.Current != null) ? System.Web.HttpContext.Current.Application : null;
+            IXpoDataStoreProvider dataStoreProvider = null;
+            if(application != null && application["DataStoreProvider"] != null) {
+                dataStoreProvider = application["DataStoreProvider"] as IXpoDataStoreProvider;
+            }
+            else {
+                dataStoreProvider = XPObjectSpaceProvider.GetDataStoreProvider(connectionString, connection, true);
+                if(application != null) {
+                    application["DataStoreProvider"] = dataStoreProvider;
+                }
+            }
+			return dataStoreProvider;
+        }
+        private void WebChartAspNetApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+            e.Updater.Update();
+            e.Handled = true;
+#else
+            if(System.Diagnostics.Debugger.IsAttached) {
+                e.Updater.Update();
+                e.Handled = true;
+            }
+            else {
+				string message = "The application cannot connect to the specified database, " +
+					"because the database doesn't exist, its version is older " +
+					"than that of the application or its schema does not match " +
+					"the ORM data model structure. To avoid this error, use one " +
+					"of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+                if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+                }
+                throw new InvalidOperationException(message);
+            }
+#endif
+        }
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule();
+            this.module3 = new WebChart.Module.WebChartModule();
+            this.module4 = new WebChart.Module.Web.WebChartAspNetModule();
+            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // WebChartAspNetApplication
+            // 
+            this.ApplicationName = "WebChart";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.objectsModule);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.WebChartAspNetApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+    }
+}
